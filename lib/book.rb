6ccbe6279fb496129cb24acb7d792244c98e02fe
Andrew Patterson <andrew.patterson747@gmail.com>
2021-07-29 15:55:45 -0500

    Complete book.rb

---
+++ lib/book.rb
@@ -0,0 +1,18 @@
+class Book
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name  = details[:author_last_name]
+    @title             = details[:title]
+    @publication_date  = details[:publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    @publication_date[-4..-1]
+  end
+end
